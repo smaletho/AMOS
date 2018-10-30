de4ff9b2a87fb4c82ec0ce454491e369104c6e9c
Ryan Kinnen <ryankinnen@gmail.com>
2018-10-30 12:12:30 -0400

    ConfigureBook: work-in-progress

---
+++ Amos/Content/js/Configure.js
@@ -0,0 +1,20 @@
+﻿
+updateItemActionsPosition();
+
+$(window).scroll(function () {
+    updateItemActionsPosition();
+});
+$(window).resize(function () {
+    updateItemActionsPosition();
+});
+
+function updateItemActionsPosition() {
+    var scrollY = $(window).scrollTop();
+    var outlineX = $('#BookOutline').position().left;
+    var outlineY = $('#BookOutline').position().top;
+    var outlineWidth = $('#BookOutline').outerWidth();
+    $('#ItemActions').css('position', 'fixed');
+    $('#ItemActions').css('left', outlineX + outlineWidth);
+    $('#ItemActions').css('top', Math.max(outlineY - scrollY, 0));
+
+}
---
+++ Amos/Controllers/ConfigureController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Amos.Controllers.Configuration
+{
+    public class ConfigureController : Controller
+    {
+        // GET: Configure
+        public ActionResult Index(int id)
+        {
+            int bookId = id;
+
+            var pageInitModel = new PageInitModel();
+            pageInitModel.PageQueryModel = new PageQueryModel();
+
+            return View("Index", pageInitModel);
+        }
+
+        public ActionResult BookOutline(PageQueryModel PageQueryModel)
+        {
+            var bookOutlineModel = new BookOutlineModel();
+
+            return View("BookOutline",bookOutlineModel);
+        }
+    }
+
+    public class PageInitModel
+    {
+        public PageQueryModel PageQueryModel { get; set; }
+    }
+
+    public class PageQueryModel
+    {
+
+    }
+
+    public class BookOutlineModel
+    {
+
+    }
+}
