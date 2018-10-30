5a3367879c39347389226b4c49266d3ac5783dd6
Ryan Kinnen <ryankinnen@gmail.com>
2018-10-30 17:29:32 -0400

    ConfigureBook:work-in-progress

--- Amos/Content/js/Configure.js
+++ Amos/Content/js/Configure.js
@@ -1,5 +1,98 @@
-﻿
-updateItemActionsPosition();
+﻿var selectedId = null;
+var selectedType = null;
+var selectedName = null;
+initPage();
+
+
+
+
+function initPage() {
+
+    updateItemActionsPosition();
+    updateOutlineInteractions();
+
+    $('#ItemActions .name-update').click(function () {
+        var data = {};
+        data.Type = selectedType;
+        data.Id = selectedId;
+        data.Name = $('#ItemActions [data-type="' + selectedType + '"] .name-input').val();
+        data.pageQueryModel = pageQueryModel;
+        //alert(data.Type + ' ' + data.Id + ' ' + data.Name);
+        transmitAction("UpdateName", updateOutlineResponse, null, 'html', data);
+        return false;
+    });
+
+
+
+}
+
+//function updateNameResponse(data) {
+//    $('#BookOutline').html(data);
+//    updateOutlineInteractions();
+//    hiliteSelectedItem();
+//}
+
+function updateOutlineInteractions() {
+    $('#BookOutline .item').click(function () {
+        selectedType = $(this).data('type');
+        selectedId = $(this).data('id');
+        selectedName = $(this).find('.name').text();
+        //itemSelected(type, id, name);
+        //selectedId = id;
+        //selectedType = type;
+        //selectedName = name;
+        hiliteSelectedItem();
+        showActionsForSelectedItem();
+    });
+}
+
+
+//function itemSelected(type, id, name) {
+//    selectedId = id;
+//    selectedType = type;
+//    selectedName = name;
+//    hiliteSelectedItem();
+//    showActionsForSelectedItem();
+//}
+
+function hiliteSelectedItem() {
+    $('#BookOutline .item').removeClass('selectedItem');
+    $('#BookOutline .item[data-type="' + selectedType + '"][data-id="' + selectedId + '"]').addClass('selectedItem');
+}
+
+function showActionsForSelectedItem() {
+    $('#ItemActions .actions-set').hide();
+    $('#ItemActions [data-type="' + selectedType + '"] .name-input').val(selectedName);
+    switch (selectedType) {
+        case "book":
+            $('#BookActions').show();
+            break;
+        case "module":
+            $('#ModuleActions').show();
+            break;
+        case "section":
+            $('#SectionActions').show();
+            break;
+        case "chapter":
+            $('#ChapterActions').show();
+            break;
+        case "page":
+            $('#PageActions').show();
+            break;
+    }
+}
+
+
+function updateOutline() {
+    transmitAction("BookOutline", updateOutlineResponse, null, 'html', pageQueryModel);
+}
+
+function updateOutlineResponse(data) {
+    $('#BookOutline').html(data);
+    updateOutlineInteractions();
+    hiliteSelectedItem();
+}
+
 
 $(window).scroll(function () {
     updateItemActionsPosition();
@@ -16,5 +109,16 @@ function updateItemActionsPosition() {
     $('#ItemActions').css('position', 'fixed');
     $('#ItemActions').css('left', outlineX + outlineWidth);
     $('#ItemActions').css('top', Math.max(outlineY - scrollY, 0));
+}
 
+function transmitAction(action, successCallback, errorCallback, returnDataType, data) {
+    $.ajax({
+        url: actionTransmitUrl.replace('action', action),
+        data: JSON.stringify(data),
+        type: 'POST',
+        dataType: returnDataType,
+        contentType: "application/json",
+        success: successCallback,
+        error: errorCallback
+    });
 }
--- Amos/Controllers/ConfigureController.cs
+++ Amos/Controllers/ConfigureController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Amos.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,7 @@ namespace Amos.Controllers.Configuration
 
             var pageInitModel = new PageInitModel();
             pageInitModel.PageQueryModel = new PageQueryModel();
+            pageInitModel.PageQueryModel.BookId = bookId;
 
             return View("Index", pageInitModel);
         }
@@ -23,7 +25,91 @@ namespace Amos.Controllers.Configuration
         {
             var bookOutlineModel = new BookOutlineModel();
 
+            var db = new ApplicationDbContext();
+
+            bookOutlineModel.OutlineBook = (from s in db.Books
+                                            where s.BookId == PageQueryModel.BookId
+                                            select new OutlineBook
+                                            {
+                                                BookId = s.BookId,
+                                                Version = s.Version,
+                                                Name = s.Name,
+                                            }).FirstOrDefault();
+
+            bookOutlineModel.OutlineModules = (from s in db.Modules
+                                               where s.BookId == PageQueryModel.BookId
+                                               orderby s.SortOrder
+                                               select new OutlineModule
+                                               {
+                                                   ModuleId = s.ModuleId,
+                                                   Name = s.Name
+                                               }).ToList();
+            var moduleIdList = (from s in bookOutlineModel.OutlineModules select s.ModuleId);
+
+            bookOutlineModel.OutlineSections = (from s in db.Sections
+                                               where moduleIdList.Contains(s.ModuleId)
+                                                orderby s.SortOrder
+                                               select new OutlineSection
+                                               {
+                                                   SectionId = s.SectionId,
+                                                   ModuleId = s.ModuleId,
+                                                   Name = s.Name
+                                               }).ToList();
+            var sectionIdList = (from s in bookOutlineModel.OutlineSections select s.SectionId);
+
+            bookOutlineModel.OutlineChapters = (from s in db.Chapters
+                                               where sectionIdList.Contains(s.SectionId)
+                                                orderby s.SortOrder
+                                               select new OutlineChapter
+                                               {
+                                                   ChapterId = s.ChapterId,
+                                                   SectionId = s.SectionId,
+                                                   Name = s.Name
+                                               }).ToList();
+
+            bookOutlineModel.OutlinePages = (from s in db.Pages
+                                               where s.BookId == PageQueryModel.BookId
+                                               orderby s.SortOrder
+                                               select new OutlinePage
+                                               {
+                                                   PageId = s.PageId,
+                                                   ChapterId = s.ChapterId,
+                                                   Title = s.Title,
+                                                   Type = s.Type,
+                                                   PageContent = s.PageContent
+                                               }).ToList();
+
             return View("BookOutline",bookOutlineModel);
+        }
+
+        public ActionResult UpdateName(UpdateNameRequest UpdateNameRequest)
+        {
+            var db = new ApplicationDbContext();
+            switch (UpdateNameRequest.Type)
+            {
+                case "book":
+                    var book = db.Books.Find(UpdateNameRequest.Id);
+                    book.Name = UpdateNameRequest.Name;
+                    break;
+                case "module":
+                    var module = db.Modules.Find(UpdateNameRequest.Id);
+                    module.Name = UpdateNameRequest.Name;
+                    break;
+                case "section":
+                    var section = db.Sections.Find(UpdateNameRequest.Id);
+                    section.Name = UpdateNameRequest.Name;
+                    break;
+                case "chapter":
+                    var chapter = db.Chapters.Find(UpdateNameRequest.Id);
+                    chapter.Name = UpdateNameRequest.Name;
+                    break;
+                case "page":
+                    var page = db.Pages.Find(UpdateNameRequest.Id);
+                    page.Title = UpdateNameRequest.Name;
+                    break;
+            }
+            db.SaveChanges();
+            return BookOutline(UpdateNameRequest.PageQueryModel);
         }
     }
 
@@ -34,11 +120,59 @@ namespace Amos.Controllers.Configuration
 
     public class PageQueryModel
     {
-
+        public int BookId { get; set; }
     }
 
     public class BookOutlineModel
     {
-
+        public OutlineBook OutlineBook { get; set; }
+        public List<OutlineModule> OutlineModules { get; set; }
+        public List<OutlineSection> OutlineSections { get; set; }
+        public List<OutlineChapter> OutlineChapters { get; set; }
+        public List<OutlinePage> OutlinePages { get; set; }
     }
+    public class OutlineBook
+    {
+        public int BookId { get; set; }
+        public string Version { get; set; }
+        public string Name { get; set; }
+    }
+    public class OutlineModule
+    {
+        public int ModuleId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class OutlineSection
+    {
+        public int SectionId { get; set; }
+        public int ModuleId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class OutlineChapter
+    {
+        public int ChapterId { get; set; }
+        public int SectionId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class OutlinePage
+    {
+        public int PageId { get; set; }
+        public int ChapterId { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string PageContent { get; set; }
+    }
+
+    public class UpdateNameRequest
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public PageQueryModel PageQueryModel { get; set; }
+    }
+
+
 }
